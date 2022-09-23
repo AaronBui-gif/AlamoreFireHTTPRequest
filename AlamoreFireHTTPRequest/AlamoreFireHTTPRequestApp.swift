f67208f33795221b4ba17d58e7fb0d91ea3b2712
AaronBui-gif <s3740934@rmit.edu.vn>
2022-09-23 16:29:56 +0700

    Initial Commit

---
+++ AlamoreFireHTTPRequest/AlamoreFireHTTPRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamoreFireHTTPRequestApp.swift
+//  AlamoreFireHTTPRequest
+//
+//  Created by Huy Bui Thanh on 23/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct AlamoreFireHTTPRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
